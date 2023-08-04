154a0beb9c1c24c68510a9b545c0583ddaf9d89c
gabicerqueira <133364342+gabicerqueira@users.noreply.github.com>
2023-08-04 16:41:21 -0300

    Initial commit

---
+++ aula.js
@@ -0,0 +1,17 @@
+// MATH RANDOM
+console.log(Math.random());
+console.log(Math.random()*20);
+console.log(Math.random(7 - 3 + 1) +3); 
+// (valor maximo - valor minimo + 1) + minimo
+
+// MATH ROUND - arredonda para o número mais próximo
+console.log(Math.round(3.1));
+console.log(Math.round(3.9));
+
+// MATH FLOOR - arredonda para baixo
+console.log(Math.floor(3.1));
+console.log(Math.floor(3.9));
+
+// MATH CEIL - arredonda para cima
+console.log(Math.ceil(3.1));
+console.log(Math.ceil(3.9));
